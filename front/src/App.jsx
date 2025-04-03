970460942c33a6d7f9fde3a98565ea904ad8c246
ichrak <ichrakbentalbi@gmail.com>
2025-04-03 22:55:18 +0100

    create-button

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <> 
     <Header />
-    <Footer/>
+    
     <Title/>
+    <Footer/>
    </>
   );
 }
--- front/src/title.jsx
+++ front/src/title.jsx
@@ -5,6 +5,11 @@ function Title() {
       <h1>Secure your 
       messages.</h1>
       <h2>let scrycto help you with that </h2>
+      <p>We provide a secure and private way to send messages. </p> 
+      
+    <button onClick={() => alert("Button Clicked!")}>Click Me</button>
+
+      
     </div>
   );
 }
