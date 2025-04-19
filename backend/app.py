ed8954ac8a409db16fa370535ac1982d2edf3e04
“manelarfi” <manelarfi@gmail.com>
2025-04-19 02:25:59 +0100

    folder structure

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    print("Hello, Flask! This is a console log.")  # This prints to the server console
-    return "Check your console!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def hello(name: str):
+    return name
