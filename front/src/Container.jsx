a8f58e84598917639d17020fb801970cb7d5267e
ichrak <ichrakbentalbi@gmail.com>
2025-04-19 22:00:37 +0100

    going insane

---
+++ front/src/Container.jsx
@@ -0,0 +1,11 @@
+
+
+function Container() {
+  return (
+    <Container className="container">
+
+    </Container >);}
+
+
+
+export default Container;
--- front/src/title.jsx
+++ front/src/title.jsx
@@ -10,7 +10,7 @@ function Title() {
       <div className="subtitle">
          <h3>We provide a secure and private way to send messages. </h3> 
       </div>
-          <button onClick={() => alert("Button Clicked!")}>Let's get started </button>
+          <button className="landin" onClick={() => alert("Button Clicked!")}>Let's get started </button>
 
       </>
   
